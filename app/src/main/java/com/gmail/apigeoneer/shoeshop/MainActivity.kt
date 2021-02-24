954661f48beb8c2b5035c27966b66bf77ce5a450
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-24 14:40:12 +0530

    Initial commit

---
+++ app/src/main/java/com/gmail/apigeoneer/shoeshop/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.gmail.apigeoneer.shoeshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var clAllShoes: ConstraintLayout
+    private lateinit var rvBoard: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clAllShoes = findViewById(R.id.all_shoes_constraint_layout)
+        rvBoard = findViewById(R.id.board_recycler_view)
+
+    }
+}
