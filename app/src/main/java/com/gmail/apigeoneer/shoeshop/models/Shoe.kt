d283efdc1371028a7a5f3e60774bf45a5a7caaa9
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-14 02:12:31 +0530

    Added vars to ShoeListViewModel.kt

--- app/src/main/java/com/gmail/apigeoneer/shoeshop/models/Shoe.kt
+++ app/src/main/java/com/gmail/apigeoneer/shoeshop/models/Shoe.kt
@@ -2,8 +2,7 @@ package com.gmail.apigeoneer.shoeshop.models
 
 data class Shoe(
     var name: String,
-    var size: String,
+    var size: Double,
     var brand: String,
-    var description: String,
-    val images: List<String> = mutableListOf()
+    var description: String
 )
--- app/src/main/java/com/gmail/apigeoneer/shoeshop/viewmodels/ShoeListViewModel.kt
+++ app/src/main/java/com/gmail/apigeoneer/shoeshop/viewmodels/ShoeListViewModel.kt
@@ -1,8 +1,35 @@
 package com.gmail.apigeoneer.shoeshop.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.gmail.apigeoneer.shoeshop.models.Shoe
 
 class ShoeListViewModel: ViewModel() {
 
+    /**
+     * from data class Shoe
+     */
+    private var shoe: Shoe? = Shoe("", 0.0, "", "")
+    private val shoes = mutableListOf<Shoe>()
 
+    /**
+     * from ShoeListFragment
+     */
+    // to observe the list of shoes added in the ShoeListFragment
+    private var _shoeList = MutableLiveData<List<Shoe>>()
+    val shoeList: LiveData<List<Shoe>>
+        get() = _shoeList
+
+    // to observe if a shoe has been added in the ShoeListFragment
+    private var _isShoeAdded = MutableLiveData<Boolean>()
+    val isShoeAdded: LiveData<Boolean>
+        get() = _isShoeAdded
+
+    fun addShoe() {
+        val newShoe = Shoe("", 0.0, "", "")
+        shoes.add(newShoe)
+        _isShoeAdded.value = true
+        _shoeList.value = shoes
+    }
 }
