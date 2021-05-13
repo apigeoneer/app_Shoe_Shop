e6ce284dea35f1734ecf7e9c4a12e5891de629fb
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-13 10:36:52 +0530

    Created shoe_list_item

---
+++ app/src/main/java/com/gmail/apigeoneer/shoeshop/models/Shoe.kt
@@ -0,0 +1,9 @@
+package com.gmail.apigeoneer.shoeshop.models
+
+data class Shoe(
+    var name: String,
+    var size: Double,
+    var brand: String,
+    var description: String,
+    val images: List<String> = mutableListOf()
+)
